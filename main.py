17f9e5aa35087826f03eceb7331c720aad8c16b8
anhphamngo <144450186+anhphamngo@users.noreply.github.com>
2023-09-08 05:53:25 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from cal.jan import *
+from cal.feb import *
+
+print("Jan")
+print("janSat: ", janSatDate)
+print("janSun: ", janSunDate)
+print()
+print("Feb")
+print("febSat: ", febSatDate)
+print("febSun: ", febSunDate)
